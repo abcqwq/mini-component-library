4fc635e78bbaf23c9698d13f8ee6fe22383c17c7
abcdefghijklqwq <butrahandisya@gmail.com>
2024-12-07 16:08:42 +0700

    finish up progress-bar

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -5,8 +5,64 @@ import styled from 'styled-components';
 import { COLORS } from '../../constants';
 import VisuallyHidden from '../VisuallyHidden';
 
-const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+const HEIGHTS = {
+    small: {
+        '--height': `${8 / 16}rem`,
+        '--bar-radius': '4px',
+    },
+    medium: {
+        '--height': `${12 / 16}rem`,
+        '--bar-radius': '4px',
+    },
+    large: {
+        '--height': `${24 / 16}rem`,
+        '--bar-radius': '8px',
+        '--padding': '4px',
+    },
 };
+
+const ProgressBar = ({ value, size }) => {
+    return (
+        <>
+            <VisuallyHidden
+                aria-valuenow={value}
+                aria-label={'progress-bar'}
+            >
+                Progress Bar: {value}%
+            </VisuallyHidden>
+            <Wrapper
+                value={value}
+                max={100}
+                style={HEIGHTS[size]}
+            ></Wrapper>
+        </>
+    );
+};
+
+const Wrapper = styled.progress`
+    appearance: none;
+    box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
+
+    height: var(--height);
+
+    &::-webkit-progress-bar {
+        background-color: ${COLORS.transparentGray15};
+        padding: var(--padding);
+
+        border-radius: var(--bar-radius);
+    }
+
+    &::-webkit-progress-value {
+        background-color: ${COLORS.primary};
+
+        border-top-left-radius: ${4 / 16}rem;
+        border-bottom-left-radius: ${4 / 16}rem;
+
+        border-top-right-radius: ${(p) =>
+            (4 / 16) * Math.max(0, p.value - 99)}rem;
+        border-bottom-right-radius: ${(p) =>
+            (4 / 16) * Math.max(0, p.value - 99)}rem;
+    }
+`;
 
 export default ProgressBar;
